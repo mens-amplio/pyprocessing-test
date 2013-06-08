7ab9641c5887f9fc47757763a2613117587a6b35
Jesse Wolfe <jes5199@gmail.com>
2013-06-08 16:22:30 -0700

    Use my new tree file instead of mixing math and drawing

--- draw_long_cylinders.py
+++ draw_long_cylinders.py
@@ -6,59 +6,26 @@ import math
 # make a network and draw it
 network = Network()
 
-f = open("../modeling/curve_endpoints.txt")
+f = open("../modeling/tree.data.py")
 data = eval(f.read())
 
-def distance_between(p1, p2):
-  return math.sqrt(sum( map( lambda(s):s*s, map( lambda(x,y):x-y, zip(p1, p2) ) ) ))
+origin = network.add_node( 0, 0, 0 )
 
-def how_long(c):
-  return distance_between(c[0], c[1])
+def draw_tree(tree, below):
+  roots = []
+  for (rod,children) in tree:
+    ((x1,y1,z1),(x2,y2,z2)) = rod
+    n1 = network.add_node( x1, y1, z1 )
+    n2 = network.add_node( x2, y2, z2 )
+    roots.append(n1)
+    network.add_edge( n1, n2, (255, 0, 0) )
+    network.add_edge( below, n1, (0, 255, 255) )
+    child_nodes = draw_tree(children, n2)
+  return roots
 
-def mean(lst):
-  return float(sum(lst))/len(lst) if len(lst) > 0 else float('nan')
+draw_tree(data, origin)
 
-# kludge to get the model sorta near the origin
-x_trans = 10
-y_trans = -150
-
-n1 = network.add_node( 0, 0, 0 )
-
-cylinders = []
-for c in data:
-  x = how_long(c)
-  if x > 17:
-    cylinders.append(c)
-
-neighborhoods = []
-neighborhood_of_point = {}
-for c in cylinders:
-  for c_point in c:
-    found_neighborhood = None
-    for neighborhood in neighborhoods:
-      if found_neighborhood:
-        break
-      for neighbor in neighborhood:
-        if distance_between(c_point, neighbor) < 3:
-          found_neighborhood = neighborhood
-          break
-    if not found_neighborhood:
-      found_neighborhood = []
-      neighborhoods.append(found_neighborhood)
-    found_neighborhood.append(c_point)
-    neighborhood_of_point[ tuple(c_point) ] = found_neighborhood
-
-neighbor_centers = []
-for neighborhood in neighborhoods:
-  neighbor_centers.append([mean(axis) for axis in zip(*neighborhood)])
-
-nodes = []
-for center in neighbor_centers:
-  nodes.append(network.add_node( center[0] - x_trans, center[1] - y_trans, center[2] ))
-
-for c in cylinders:
-  n = [nodes[neighborhoods.index(neighborhood_of_point[tuple(cp)])] for cp in c]
-  network.add_edge( n[0], n[1], (255,0,0) )
+#network.add_edge( n[0], n[1], (255,0,0) )
 
 print("ok")
 
